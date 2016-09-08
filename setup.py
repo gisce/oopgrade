1484dcfcd5240bb728bf9eb4cb076f8aed0065af
Eduard Carreras <ecarreras@gisce.net>
2016-09-08 10:25:16 +0200

    Bump to v0.6.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dirname = os.path.dirname(__file__)
 
 setup(
     name='oopgrade',
-    version='0.5.0',
+    version='0.6.0',
     description='Upgrade and migration tools',
     long_description=readme,
     author='GISCE-TI, S.L.',
