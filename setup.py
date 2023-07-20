d82e38a3df71376dd612d7b7c2b8bff2cd6345f4
PSala <polsalalidon@gmail.com>
2023-07-20 09:50:24 +0200

    Bump to v0.14.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dirname = os.path.dirname(__file__)
 
 setup(
     name='oopgrade',
-    version='0.14.1',
+    version='0.14.2',
     description='Upgrade and migration tools',
     long_description=readme,
     author='GISCE-TI, S.L.',
