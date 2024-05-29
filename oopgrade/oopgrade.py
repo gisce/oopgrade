b0040707de49b3938ab0d7f407834f0aefadb77e
Nil Villarroya <nvillarroya@gisce.net>
2024-05-29 16:05:14 +0200

    fix delete_record

--- oopgrade/oopgrade.py
+++ oopgrade/oopgrade.py
@@ -64,7 +64,7 @@ def delete_record(cursor, module_name, record_names):
             cursor.execute(sql_model_del, params_model_del)
 
             model_o = pool.get(model_data_vs['model'])
-            model_o.unlink(cursor, uid, model_data_vs['res_id'])
+            model_o.unlink(cursor, uid, [model_data_vs['res_id']])
         elif model_data_vs and len(model_data_vs) > 1:
             raise Exception(
                 "More than one record found for model %s" % (model_data_vs['model'])
