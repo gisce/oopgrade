ea5a24e5c12dd7438869694f31832e466130f9f3
Vilaboa <mvilaboa@gisce.net>
2024-07-18 11:16:37 +0200

    FIX tests

--- spec/data_migration_spec.py
+++ spec/data_migration_spec.py
@@ -186,9 +186,11 @@ with description('Migrating _data.xml'):
             }
             add_columns(cursor, columns, multiple=True)
             expected_sql = [
-                call('SELECT count(attname) FROM pg_attribute WHERE attrelid = ( SELECT oid FROM pg_class WHERE relname = %s ) AND attname = %s', ('test_model', 'random1')),
                 call(
-                    'SELECT count(attname) FROM pg_attribute WHERE attrelid = ( SELECT oid FROM pg_class WHERE relname = %s ) AND attname = %s',
+                    'SELECT count(attname) FROM pg_attribute WHERE attrelid = (SELECT oid FROM pg_class WHERE relname = %s AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = "public") AND relkind = "r") AND attname = %s',
+                    ('test_model', 'random1')),
+                call(
+                    'SELECT count(attname) FROM pg_attribute WHERE attrelid = (SELECT oid FROM pg_class WHERE relname = %s AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = "public") AND relkind = "r") AND attname = %s',
                     ('test_model', 'random2')),
                 call(
                     'ALTER TABLE "test_model" ADD COLUMN "random1" character varying(16),\nADD COLUMN "random2" character varying(16)'),
@@ -213,12 +215,12 @@ with description('Migrating _data.xml'):
             add_columns(cursor, columns, multiple=False)
             expected_sql = [
                 call(
-                    'SELECT count(attname) FROM pg_attribute WHERE attrelid = ( SELECT oid FROM pg_class WHERE relname = %s ) AND attname = %s',
+                    'SELECT count(attname) FROM pg_attribute WHERE attrelid = (SELECT oid FROM pg_class WHERE relname = %s AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = "public") AND relkind = "r") AND attname = %s',
                     ('test_model', 'random1')),
                 call(
                     'ALTER TABLE "test_model" ADD COLUMN "random1" character varying(16)'),
                 call(
-                    'SELECT count(attname) FROM pg_attribute WHERE attrelid = ( SELECT oid FROM pg_class WHERE relname = %s ) AND attname = %s',
+                    'SELECT count(attname) FROM pg_attribute WHERE attrelid = (SELECT oid FROM pg_class WHERE relname = %s AND relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = "public") AND relkind = "r") AND attname = %s',
                     ('test_model', 'random2')),
                 call(
                     'ALTER TABLE "test_model" ADD COLUMN "random2" character varying(16)'),
