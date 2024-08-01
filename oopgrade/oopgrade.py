439daffd237e13025d809b440c0c7bf4acc8f0b1
eberloso <eberloso@gisce.net>
2024-08-01 11:09:06 +0200

    FIx if

--- oopgrade/oopgrade.py
+++ oopgrade/oopgrade.py
@@ -759,6 +759,6 @@ def load_translation(cursor, lang, name, type, res_id, src, value):
         insert_sql = """
         INSERT INTO ir_translation(lang, name, type, res_id, src, value) 
         VALUES (%(lang)s, %(name)s, %(type)s, %(res_id)s, %(src)s, %(value)s) 
-        ON CONFLICT (lang, src_md5, name, type, res_id) WHERE res_id is not null DO UPDATE SET value = EXCLUDED.value
+        ON CONFLICT (lang, src_md5, name, type, res_id) WHERE res_id is null DO UPDATE SET value = EXCLUDED.value
         """
     cursor.execute(insert_sql, {'lang': lang, 'name': name, 'type': type, 'res_id': res_id, 'src': src, 'value': value})
