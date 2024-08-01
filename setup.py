b1617e5f62e451e3cef32b61b07fa63da4a055e5
Release <github-actions@github.com>
2024-08-01 12:33:21 +0200

    Bump to v0.17.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dirname = os.path.dirname(__file__)
 
 setup(
     name='oopgrade',
-    version='0.16.3',
+    version='0.17.0',
     description='Upgrade and migration tools',
     long_description=readme,
     author='GISCE-TI, S.L.',
