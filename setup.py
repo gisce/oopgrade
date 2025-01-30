dc1be88363571965a7b6756f867b91b4271bbead
Release <github-actions@github.com>
2025-01-30 12:33:38 +0100

    Bump to v0.18.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dirname = os.path.dirname(__file__)
 
 setup(
     name='oopgrade',
-    version='0.18.0',
+    version='0.18.1',
     description='Upgrade and migration tools',
     long_description=readme,
     long_description_content_type='text/x-rst',
