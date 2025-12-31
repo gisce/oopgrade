9bd31f60440d1c9ade5ba266e46f4e732c03725a
Release <github-actions@github.com>
2025-12-31 09:47:41 +0100

    Bump to v0.25.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ dirname = os.path.dirname(__file__)
 
 setup(
     name='oopgrade',
-    version='0.24.0',
+    version='0.25.0',
     description='Upgrade and migration tools',
     long_description=readme,
     long_description_content_type="text/markdown",
